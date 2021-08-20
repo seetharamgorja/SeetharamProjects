32564c1af2159af86e037aefcd831ecbd211ded4
GorjaS <GorjaS@PENA4-VSP-045.pena4tech.net>
2021-08-20 16:03:39 +0530

    test

---
+++ JenkinsTest/src/test/java/example/NewTest.java
@@ -0,0 +1,28 @@
+package example;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	public WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
+		 driver = new ChromeDriver();
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}		
+}
